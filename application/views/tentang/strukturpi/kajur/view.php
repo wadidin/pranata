ca91472ada1287022af752cc6ed31f0ba60e3556
agungribowo <ribowoagung@gmail.com>
2017-04-20 23:28:48 +0700

    struktur

---
+++ application/views/tentang/strukturpi/kajur/view.php
@@ -0,0 +1,58 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+
+<!-- Page Title -->
+<title>Ketua Jurusan | STMIK Pranata Indonesia</title>
+
+
+
+  <!-- Start main-content -->
+  <div class="main-content">
+    <!-- Section: inner-header -->
+    <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="<?php echo base_url(); ?>assets/depan/images/home/slider/slider-3.jpg">
+      <div class="container pt-70 pb-20">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="title text-white">Ketua Jurusan</h2>
+              <ol class="breadcrumb text-left text-black mt-10">
+                <li><a href="javascript:void(0)">Tentang</a></li>
+                <li><a href="javascript:void(0)">Struktur PI</a></li>
+                <li class="active text-gray-silver">Ketua Jurusan</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Section: Have Any Question -->
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-md-push-1">
+            <div id="section-one" class="mb-50">
+              <h3><b>Ketua Jurusan STMIK Pranata Indonesia</b></h3>
+              <hr>
+              <p class="mb-20">
+              <p align="justify"><b>Sekolah Tinggi Manajemen Informatika dan Komputer (STMIK) Pranata Indonesia</b> di selenggarakan oleh <b><i>Yayasan Manajemen Informatika Indonesia (YMII)</i></b>, tumbuh dan berkembang berawal dari lahirnya <b><i>Lembaga Pendidikan Manajemen Informatika dan Komputer (LPMIK)</i></b> <b><i>YMII Cipto Hadi Pranoto</i></b> sebagai sosok lembaga pendidikan profesi yang bernaung dibawah <b>DIKLUSEMAS DEPDIKNAS</b>. Didirikan pada tanggal 27 Juli tahun 1996 di jalan Cut Mutiah No.28 A-B Bekasi Timur.</p><br>
+
+              <p align="justify">Eksistensi dua bersaudara begitu padu dan harmonis dalam olah manajemen. Drs. Yusrodi sebagai pimpinan tertinggi dan Drs. Suwandi sebagai pemimpin operasional, dalam tahun pertama mampu merekrut 368 mahasiswa dan pada tahun ke-dua meningkat menjadi 568 mahasiswa. Secara kuantitas angka ini menunjukkan bahwa Lpmik Cipto Hadi Pranoto dapat diterima oleh masyarakat Bekasi.</p><br>
+
+              <p align="justify">Seiring dengan perkembangan lembaga dan keinginan meningkatkan status LPMIK, pada 01 Juni 1998 Drs. Suwandi dipanggil menghadap sang khalik (meninggal dunia dalam usia 37 tahun). Sejak saat itu, duka yang mendalam dengan semangat yang tak pernah luntur, Drs. Yusrodi terus melangkah maju.</p><br>
+
+              <p align="justify">Pesatnya perkembangan jumlah mahasiswa diiringi dengan meningkatkan minat dan keinginan masyarakat terhadap jalur pendidikan tinggi, mendorong ketua Yayasan Manajemen Informatika Indonesia (YMII) sebagai penyelenggara meningkatkan status Lpmik menjadi STMIK. Peristiwa ini terjadi pada tahun ke-tiga tepatnya pada bulan April 1999.</p><br>
+
+              <p align="justify">Program dan jurusan yang pertama dibuka adalah program D III dan D I jurusan Manajemen Informatika dan komputer Akuntansi, Program Sarjana Strata I jurusan Teknik Informatika serta Sistem Informasi.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+  <!-- end main-content -->
+
---
+++ application/views/tentang/strukturpi/pusatadministrasi/view.php
@@ -0,0 +1,216 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+
+<!-- Page Title -->
+<title>Pusat Administrasi | STMIK Pranata Indonesia</title>
+
+
+
+  <!-- Start main-content -->
+  <div class="main-content">
+    <!-- Section: inner-header -->
+    <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="<?php echo base_url(); ?>assets/depan/images/home/slider/slider-3.jpg">
+      <div class="container pt-70 pb-20">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="title text-white">Pusat Administrasi</h2>
+              <ol class="breadcrumb text-left text-black mt-10">
+                <li><a href="javascript:void(0)">Tentang</a></li>
+                <li><a href="javascript:void(0)">Struktur PI</a></li>
+                <li class="active text-gray-silver">Pusat Administrasi</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Section: Have Any Question -->
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-md-push-1">
+            <div id="section-one" class="mb-50">
+              <h3><b>Pusat Administrasi STMIK Pranata Indonesia</b></h3>
+              <hr>
+              <p class="mb-20">
+              <ol type="1">
+                  <h4><b><li>Pelayanan Administrasi Mahasiswa</li></b></h4>
+                  <p align="justify">Pelayanan administrasi Mahasiswa merupakan pusat layanan penampungan dan Kemudian di sampaikan Ke BAAK untuk diproses dan dapat diambil oleh Mahasiswa paling lama 4 jam kerja.</p>
+                  <ol type="1">
+                    <li>Kartu Mahasiswa</li>
+                    <li>Kartu Perpustakaan</li>
+                    <li>Kartu Laboratorium</li>
+                    <li>Kartu Rencana Studi (KRS)</li>
+                    <li>Kartu Hasil Studi</li>
+                    <li>Transkrip Akademik</li>
+                    <li>Kartu Peserta Ujian</li>
+                    <li>Surat tanda lulus Sementara</li>
+                    <li>Pengajuan pindah jurusan</li>
+                    <li>Pengajuan pindah kelas</li>
+                    <li>Pengajuan Konfersi</li>
+                    <li>Legalisir ijazah</li>
+                    <li>Menerima permohonan cuti akademik</li>
+                    <li>Menerima permohonan pembuatan surat keterangan</li>
+                    <li>Menerima pendaftaran ujian HER</li>
+                    <li>Pengajuan cuti kuliah</li>
+                  </ol>
+
+                  <h4><b><li>Dosen pembimbingan Akademik (PA)</li></b></h4>
+                  <p align="justify">Dosen pembimbing Akademik (PA) adalah tenaga edukatif Sekolah Tinggi dan Akademik Pranata Indonesia yang dituju dan diberi tugas dan tanggung jawab untuk membimbing sejumlah mahasiswa tertentu dan bertujuan untuk membantu menyelesaikan studi sesuai dengan waktu yang telah ditentukan agar mereka dapat mengembangkan potensinya dan memperoleh hasil yang optimal dalam proses belajar mengajar. Pembimbing Akademik bertanggung jawab kepada Ketua jurusan.</p>
+                  <p align="justify">Tugas dan Kewajiban Dosen pembimbing Akademik (PA) ini adalah :
+                    <ol type="1">
+                      <li>Pembimbing Akademik wajib mengetahui data-data pribadi mahasiswa asuhannya, dan mengadakan konsultasi secara rutin sesuai dengan jadwal yang telah ditentukan.</li>
+                      <li>Mempersiapkan perkembangan akademik dan non-akademik mahasiswa asuhannya. Memberi penarahan kepada mahasiswa asuhannya bila Indeks prestasinya jelek atau rendah.</li>
+                      <li>Dalam pelaksanaan mengisi kartu Rencana Studi (KRS), pembimbing Akademik wajib memeriksa Form FRS mahasiswa asuhannya tersebut sesuai ketentuan yang berlaku.</li>
+                      <li>Membuat laporan terutama apabila ada masalah yang rumit dan perlu penanganan serius.</li>
+                      <li>Memberi dorongan serta memotivasikan mahasiswa yang punya masalah dengan perkembangan akademik, seperti :
+                        <ol type="a">
+                          <li>Mahasiswa yang nilainya terlalu rendah dan banyak gagal.</li>
+                          <li>Mahasiswa yang sangat sibuk bekerja dan sulit menyediakan waktu.</li>
+                          <li>Mahasiswa yang ekonominya lemah,dan punya masalah keluarga yang tentu mempengaruhi proses belajar mengajarnya.</li>
+                        </ol>
+                      </li>
+                      <li>Pembimbing Akademik diwajibkan hadir bila ada rapat yang berkenaan dengan masalah Akademik oleh Pimpinan Sekolah Tinggi dan Akademik Pranata Indonesia.</li>
+                      <li>Pimpinan Sekolah Tinggi dan Akademi Pranata Indonesia akan mengevaluasi kehadiran dan kegiatan Dosen Pembimbing Akademik setiap semester.</li>
+                    </ol>
+                  </p>
+
+                  <h4><b><li>Biro Administrasi Akademik dan Kemahasiswaan (BAAK)</li></b></h4>
+                  <ol type="A">
+                    <h6><b><li>Bagian Registrasi</li></b></h6>
+                      <ol type="1">
+                        <li>Melaksanakan pendaftaran/registrasi</li>
+                        <li>Membuat kartu Mahasiswa</li>
+                        <li>Membuat buku induk Mahasiswa</li>
+                        <li>Mengurus NPM/Buku NPM</li>
+                        <li>Membuat daftar Mahasiswa aktif kembali</li>
+                        <li>Mengurus Mahasiswa pindahan</li>
+                        <li>Membuat daftar dan memproses cuti akademik</li>
+                        <li>Menyiapkan administrasi pindah kuliah/mutasi</li>
+                        <li>Membuat daftar Mahasiswa putus kuliah</li>
+                        <li>Membuat daftar Mahasiswa yang meninggal dunia</li>
+                        <li>Membuat laporan kepada Kepala Bagian Administrasi Akademik.</li>
+                      </ol>
+
+                    <h6><b><li>Bagian Evaluasi</li></b></h6>
+                      <ol type="1">
+                        <li>Menghimpun KRS
+                          <ol type="a">
+                            <li>Rekapitulasi setiap semester</li>
+                            <li>Registrasi tiap jurusan</li>
+                          </ol>
+                        </li>
+                        <li>Menghimpun KHS
+                          <ol type="a">
+                            <li>Rekapitulasi setiap semester</li>
+                            <li>Registrasi tiap jurusan</li>
+                          </ol>
+                        </li>
+                        <li>Menghimpun kartu hasil ujian Kompetensi
+                          <ol type="a">
+                            <li>Rekapitulasi setiap semester</li>
+                            <li>Registrasi tiap jurusan</li>
+                          </ol>
+                        </li>
+                        <li>Pembuatan buku Yudisium jurusan</li>
+                        <li>Pembuatan transkrip ujian negara dan pembatalan studi</li>
+                        <li>Membuat laporan kepada kepala bagian Administrasi Akademik.</li>
+                      </ol>
+
+                    <h6><b><li>Bagian Akreditasi</li></b></h6>
+                      <ol type="1">
+                        <li>Menghimpun data akreditasi status untuk jurusan</li>
+                        <li>Pengadaan blanko Ijazah</li>
+                        <li>Penulisan Ijazah</li>
+                        <li>Legalisir Ijazah</li>
+                        <li>Penyerahan Ijazah</li>
+                        <li>Pembuatan surat keterangan lulus sementara</li>
+                        <li>Pembuatan surat rekomendasi untuk melanjutkan studi ke S2</li>
+                        <li>Membuat laporan kepada Kepala bagian administrasi akademik</li>
+                      </ol>
+
+                    <h6><b><li>Bagian Perencanaan</li></b></h6>
+                      <ol type="1">
+                        <li>Membuka perencanaan tahun akademik
+                          <ol type="a">
+                            <li>Kalender Akademik</li>
+                            <li>Jadwal Perkuliahan / Praktikum</li>
+                            <li>Jadwal Bimbingan dan Penasehat akademik</li>
+                            <li>Jadwal Ujian-Ujian</li>
+                            <li>Penyelenggaraan Penataran</li>
+                          </ol>
+                        </li>
+                        <li>Melaksanakan monitoring proses belajar mengajar
+                          <ol type="a">
+                            <li>Rekapitulasi kehadiran dosen</li>
+                            <li>Rekapitulasi kehadiran mahasiswa</li>
+                            <li>Pelayanan akademik (Tata Usaha)</li>
+                          </ol>
+                        </li>
+                        <li>Menyusun penyempurnaan sistem informasi akademik</li>
+                        <li>Penyusun buku pedoman</li>
+                        <li>Membuat laporan Kepada Kepala kepala bagian perencanaan dan informatika</li>
+                      </ol>
+
+                    <h6><b><li>Bagian Informatika</li></b></h6>
+                      <ol type="1">
+                        <li>Memberikan layanan mengenai Sistem Informasi Akademik (SIA)</li>
+                        <li>Melaksanakan pembuatan statistik
+                          <ol type="a">
+                            <li>Mahasiswa lama baru</li>
+                            <li>Dosen tetap / dosen tidak tetap</li>
+                            <li>Lulusan tiap jurusan</li>
+                            <li>Mahasiswa penerima beasiswa</li>
+                            <li>Rasio mahasiswa, dosen, dan karyawan</li>
+                            <li>Wisudawan dan alumni</li>
+                            <li>Kegiatan penelitian dan pengabdian pada masyarakat</li>
+                          </ol>
+                        </li>
+                        <li>Membuat laporan kepada kepala Bagian Perencanaan dan Informatika</li>
+                      </ol>
+
+                    <h6><b><li>Bagian Kemahasiswaan dan alumni</li></b></h6>
+                      <ol type="1">
+                        <li>Membuat daftar alamat mahasiswa dan alumni</li>
+                        <li>Menghimpun data kegiatan mahasiswa dan beasiswa</li>
+                        <li>Memberikan pelayanan pada alumni</li>
+                        <li>Memberikan pelayanan seleksi PMB</li>
+                        <li>Memberikan pelayanan pelaksanaan MPLK</li>
+                        <li>Memberikan pelayanan pelaksanaan penataran</li>
+                        <li>Membuat laporan kepada kepala Bagian perencanaan dan informatika</li>
+                      </ol>
+                    
+                    <h6><b><li>Pelayanan Mahasiswa</li></b></h6>
+                      <ol type="1">
+                        <li>Customer Service</li>
+                        <li>Informasi penerimaan Mahasiswa Baru</li>
+                        <li>Informasi program Kursus</li>
+                        <li>Layanan jaringan <b>INTERNET</b> kemahasiswaan</li>
+                      </ol>
+
+                  </ol>
+
+                  <h4><b><li>Administrasi Keuangan</li></b></h4>
+                    <h6><b>Prosedur Biaya pendidikan</b></h6>
+                      <ol type="1">
+                        <li>Biaya Pendidikan dibayar melalui bank Yang ditunjuk</li>
+                        <li>Semua pembayaran menggunakan Slip Bang Yang ditunjuk</li>
+                        <li>Syarat pembuktian pembayaran disampaikan kepada bagian keuangan kelembagaan dan bagian keuangan kelembagaan akan melegalisasi pembayaran mahasiswa melalui kartu SPP</li>
+                        <li>Bagian keuangan lembaga akan melakukan cek kepada rekening Koran pembayaran mahasiswa</li>
+                        <li>Semua Slip pembayaran Bank agar disimpan oleh mahasiswa sebagai syarat administrasi kelulusan.</li>
+                      </ol>
+                </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+  <!-- end main-content -->
+
